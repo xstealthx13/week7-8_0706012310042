a0ea88aade177d8042d1b0e8630e7bd01d5d11d2
Nathaniel michael <153161694+xstealthx13@users.noreply.github.com>
2025-11-01 16:09:11 +0700

    Add files via upload

--- week78_0706012310042_soal2/app/src/main/java/com/example/week78_0706012310042_soal2/ui/view/soal2.kt
+++ week78_0706012310042_soal2/app/src/main/java/com/example/week78_0706012310042_soal2/ui/view/soal2.kt
@@ -1,2 +1,34 @@
 package com.example.week78_0706012310042_soal2.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.week78_0706012310042_soal2.R
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun soal2(){}
